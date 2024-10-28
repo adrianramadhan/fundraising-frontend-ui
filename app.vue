4d975b9e0632e37cd937940c47d81beee41dfd47
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-10-28 22:49:26 +0700

    feat: slicing navbar component

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
+
 <template>
   <div class="">
+    <Navbar />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
