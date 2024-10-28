f8be12f7da5ede034e9719fea559fd799dada4ca
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-10-28 22:39:11 +0700

    chore: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
+        },
+      ],
+    },
+  },
+});
